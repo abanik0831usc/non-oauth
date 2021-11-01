793dea7b513639a3f43e2cbe00fdba74791295ec
abanik <arnab_banik@intuit.com>
2021-10-31 23:39:39 -0700

    feat: add non ouath mocks

--- src/screens/success/index.js
+++ src/screens/success/index.js
@@ -3,13 +3,14 @@ import styled from "styled-components";
 
 const Div = styled.div`
   padding: 0;
+  height: 100vh;
 `
 
 class Success extends Component {
     render() {
         return (
             <Div>
-                <label style={{ display: "flex", justifyContent: 'center'}}>successfully connected to bank & data sent to IDX</label>
+                <label style={{ display: "flex", height: '100%', justifyContent: 'center', alignItems: 'center' }}>successfully connected to bank & data sent to IDX</label>
             </Div>
         );
     }
